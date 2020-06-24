1f2fa2306d004cdeea9c06bd7125926eb23da8cf
Viata Fredericks <viataruth@gmail.com>
2020-06-24 22:59:30 +0200

    Add Technicians

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -1,6 +1,6 @@
 import {
   GET_TECHS,
-  ADD_TECHS,
+  ADD_TECH,
   DELETE_TECH,
   SET_LOADING,
   TECHS_ERROR,
@@ -16,6 +16,32 @@ export const getTechs = () => async (dispatch) => {
 
     dispatch({
       type: GET_TECHS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText,
+    });
+  }
+};
+
+// Add technician to server
+export const addTech = (tech) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch("/techs", {
+      method: "POST",
+      body: JSON.stringify(tech),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_TECH,
       payload: data,
     });
   } catch (err) {
